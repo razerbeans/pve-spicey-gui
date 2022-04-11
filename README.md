be7da677f51ddf77695b54ff060e931c6e5d8b44
Randall <razerbeans@gmail.com>
2022-04-10 20:57:43 -0700

    Fully functional GUI for connecting to hosts via SPICE

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+# Installation
+
+## Everyone (See "Development" below)
+
+## Raspberry Pi
+To run this on Raspberry Pi, you need to actually use system python and pip to install the PySide2 package. To do this, run the following command:
+
+```
+sudo apt install python3-pyside2.qtgui
+pip install -r requirements.txt
+```
+
+It will error out on PySide2, but that's fine since you've installed it system-wide. Now you can run the following:
+
+```
+# CLI
+python -m pve_vdi_client <pve_vmid>
+# GUI
+python -m pve_vdi_client.vdi_gui.gui
+```
+
 # Development
 
 Run the following to get things set up:
--- pve_vdi_client/vdi_gui/gui.py
+++ pve_vdi_client/vdi_gui/gui.py
@@ -2,8 +2,8 @@
 
 import sys
 
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import (QApplication, QPushButton, QComboBox, QDialog,
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import (QApplication, QPushButton, QComboBox, QDialog,
                                QDialogButtonBox, QGridLayout, QGroupBox,
                                QFormLayout, QHBoxLayout, QLabel, QLineEdit,
                                QMenu, QMenuBar, QPushButton, QSpinBox,
@@ -23,17 +23,20 @@ class Gui(QDialog):
     password_box = QLineEdit()
     password_box.setEchoMode(QLineEdit.Password)
     self._password_input = password_box
+    
     self._fetch_button = QPushButton("Fetch VMs")
     self._fetch_button.clicked.connect(self._set_client)
+
     self._vm_dropdown = QComboBox()
     self._vm_dropdown.setDisabled(True)
-    self._connect_button = QPushButton("Connect")
-    self._connect_button.setDisabled(True)
+    
     self._client = None
 
-    button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+    self._okay_button = QDialogButtonBox.Ok
 
-    button_box.accepted.connect(self.accept)
+    button_box = QDialogButtonBox(self._okay_button | QDialogButtonBox.Cancel)
+
+    button_box.accepted.connect(self._connect_to_vm)
     button_box.rejected.connect(self.reject)
 
     main_layout = QVBoxLayout()
@@ -59,7 +62,6 @@ class Gui(QDialog):
     _horizontal_group_box = QGroupBox("VMs")
     layout = QFormLayout()
     layout.addRow(QLabel("VM: "), self._vm_dropdown)
-    layout.addRow(self._connect_button)
 
     _horizontal_group_box.setLayout(layout)
     return _horizontal_group_box
@@ -67,8 +69,9 @@ class Gui(QDialog):
 
   def _fetch_vms(self):
     vms = self._client.cluster_vms()
-    self._vm_dropdown.addItems(sorted(["{} - {}".format(vm['vmid'], vm['name']) for vm in vms]))
+    self._vm_dropdown.addItems(sorted(["{}-{}".format(vm['vmid'], vm['name']) for vm in vms]))
     self._vm_dropdown.setEnabled(True)
+    self.button_box.accepted.setEnabled(True)
 
   def _set_client(self):
     self._client = Client(host=self._server_input.text(),
@@ -76,6 +79,10 @@ class Gui(QDialog):
                           password=self._password_input.text())
     self._fetch_vms()
     return None
+
+  def _connect_to_vm(self):
+    vm_id = int(self._vm_dropdown.currentText().split("-")[0])
+    self._client.spice_connect(vmid=vm_id)
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
